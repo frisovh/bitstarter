6148247217d83e3ee5b4b709fb7a5d91f29d4869
Ubuntu <ubuntu@ip-172-31-26-3.eu-central-1.compute.internal>
2016-02-15 20:56:22 +0000

    Includes POST

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 var express = require('express')
 var fs = require('fs')
+var bodyParser = require('body-parser')
+
 var app = express()
-
-
 var buf = fs.readFileSync('index.html')
 var indexfile2 = buf.toString(indexfile2, "utf-8")
 
-
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
-/*app.get('/', function(request, response) {
-  response.send(indexfile2)
-})*/
 
  /* serves all the static files */
  app.get(/^(.+)$/, function(req, res){ 
@@ -20,6 +15,13 @@ app.use(express.static(__dirname + '/public'))
      res.sendfile( __dirname + req.params[0]); 
  });
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(function (req, res) {
+  var post_data = req.body;
+  console.log(post_data);
+})
 
 
 app.listen(app.get('port'), function() {
