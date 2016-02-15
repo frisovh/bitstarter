85feea8e3e452fc4495ae61d611c33ee5beaf62c
Ubuntu <ubuntu@ip-172-31-26-3.eu-central-1.compute.internal>
2016-02-15 16:44:10 +0000

    Bootstrapped layout

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ var indexfile2 = buf.toString(indexfile2, "utf-8")
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+/*app.get('/', function(request, response) {
   response.send(indexfile2)
-})
+})*/
+
+ /* serves all the static files */
+ app.get(/^(.+)$/, function(req, res){ 
+     console.log('static file request : ' + req.params);
+     res.sendfile( __dirname + req.params[0]); 
+ });
+
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
