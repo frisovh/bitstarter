74d513541fa769fc27a7899291fa1c08339ff04f
name <frisovh@gmail.com>
2016-02-16 20:54:15 +0000

    And revert

--- index.js
+++ index.js
@@ -16,6 +16,5 @@ app.use(express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+  console.log(process.env.IP)
 })
-//bla
-//bla2
